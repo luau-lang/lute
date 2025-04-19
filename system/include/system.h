57b6feb7d955857183bda1ceb17f3f8d634c53c7
ffrostfall <ffrostfall@gmail.com>
2025-04-19 17:30:02 -0400

    Make system base

---
+++ system/include/system.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "lua.h"
+#include "lualib.h"
+
+// open the library as a standard global luau library
+int luaopen_system(lua_State* L);
+// open the library as a table on top of the stack
+int luteopen_system(lua_State* L);
+
+namespace system
+{
+
+static const luaL_Reg lib[] = {
+    {nullptr, nullptr}
+};
+
+} // namespace system
