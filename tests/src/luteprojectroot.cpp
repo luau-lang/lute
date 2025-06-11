d37a4bdb1d90134d7a2c2db3064d6220ce4eba8e
Varun Saini <61795485+vrn-sn@users.noreply.github.com>
2025-06-11 04:57:28 -0700

    Add require-by-string tests

--- tests/src/luteprojectroot.cpp
+++ tests/src/luteprojectroot.cpp
@@ -4,7 +4,7 @@
 
 #include "doctest.h"
 
-std::string getLuteProjectRoot()
+std::string getLuteProjectRootAbsolute()
 {
     std::optional<std::string> cwd = getCurrentWorkingDirectory();
     REQUIRE(cwd);
@@ -21,6 +21,24 @@ std::string getLuteProjectRoot()
         std::optional<std::string> parentPath = getParentPath(luteDir);
         REQUIRE_MESSAGE(parentPath, "Error getting parent path");
         luteDir = *parentPath;
+    }
+
+    REQUIRE_MESSAGE(false, "Error getting Lute project path");
+    return "";
+}
+
+std::string getLuteProjectRootRelative()
+{
+    std::string luteDir = "./";
+
+    for (int i = 0; i < 20; ++i)
+    {
+        if (isFile(joinPaths(luteDir, ".LUTE_SENTINEL")))
+        {
+            return luteDir;
+        }
+
+        luteDir = normalizePath(luteDir + "/..");
     }
 
     REQUIRE_MESSAGE(false, "Error getting Lute project path");
