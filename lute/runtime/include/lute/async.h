083f9a383c1b2e31acf2c6dd8b775f120f005b38
bjcscat <bjcs.rbx@gmail.com>
2025-08-31 17:47:30 -0500

    Add utility classes to extend over libuv operations

---
+++ lute/runtime/include/lute/async.h
@@ -0,0 +1,29 @@
+#pragma once
+
+template<typename T>
+class UVAsyncOperation
+{
+public:
+    UVAsyncOperation()
+        : req{}
+    {
+        req.data = this;
+    }
+
+    virtual void callback() = 0;
+
+    T* getRequest()
+    {
+        return &req;
+    }
+
+    static void uv_async_callback(T* req)
+    {
+        auto* operation = static_cast<UVAsyncOperation*>(req->data);
+
+        operation->callback();
+    }
+
+private:
+    T req;
+};
