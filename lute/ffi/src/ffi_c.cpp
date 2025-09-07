3e01232677b9bb42811340ba188a61b53265b80b
green-real <obviouslygreen.official@gmail.com>
2025-09-07 10:02:32 -0700

    Removed test function from FFI library

--- lute/ffi/src/ffi_c.cpp
+++ lute/ffi/src/ffi_c.cpp
@@ -3,8 +3,6 @@
 #include "lua.h"
 #include "lualib.h"
 
-#include "ffi.h"
-
 #include <array>
 
 int add(int a, int b)
@@ -15,37 +13,7 @@ int add(int a, int b)
 namespace ffi
 {
 
-int lua_ctestadd(lua_State* L)
-{
-    int a = luaL_checkinteger(L, 1);
-    int b = luaL_checkinteger(L, 2);
-
-    ffi_cif cif;
-    ffi_type* types[2];
-
-    types[0] = &ffi_type_sint;
-    types[1] = &ffi_type_sint;
-
-    if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 2, &ffi_type_sint, types) != FFI_OK)
-    {
-        luaL_error(L, "could not prepare call interface");
-    }
-
-    void* values[2];
-    int result;
-
-    values[0] = &a;
-    values[1] = &b;
-
-    ffi_call(&cif, FFI_FN(add), &result, values);
-
-    lua_pushinteger(L, result);
-    return 1;
-}
-
 static const luaL_Reg clib[] = {
-    {"testadd", lua_ctestadd},
-
     {nullptr, nullptr}
 };
 
