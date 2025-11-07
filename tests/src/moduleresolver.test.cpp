3c9437b355e27bb3ff3358f8d8b056863b926361
Annie Tang <98965493+annieetang@users.noreply.github.com>
2025-11-06 17:11:35 -0800

    format and remove wip tests

--- tests/src/moduleresolver.test.cpp
+++ tests/src/moduleresolver.test.cpp
@@ -21,48 +21,4 @@ TEST_CASE("moduleresolver_read_source")
     CHECK(source->source.find("require") != std::string::npos);
 }
 
-// this is not working i'll fix tmr
-TEST_CASE("moduleresolver_resolve_module_simple")
-{
-    std::string root = getLuteProjectRootAbsolute();
-    std::string requirer = joinPaths(root, "tests/src/resolver/types.luau");
-    Luau::LuteModuleResolver resolver;
-
-    // Create an AST arena to allocate AST nodes properly
-    Luau::AstNameTable names;
-    Luau::AstArena arena;
-
-    // Build a constant string node
-    std::string requirePath = "./types";
-    Luau::AstString astString{requirePath.data(), static_cast<unsigned>(requirePath.size())};
-    Luau::Location loc{0, 0}; // not important for this test
-
-    Luau::AstExprConstantString* requireExpr = arena.alloc<Luau::AstExprConstantString>(loc, astString);
-
-    // Now call the resolver
-    auto moduleInfoOpt = resolver.resolveModule(&context, requireExpr);
-    REQUIRE(moduleInfoOpt);
-    Luau::ModuleInfo moduleInfo = *moduleInfoOpt;
-    CHECK(moduleInfo.name.find("types.luau") != std::string::npos);
-}
-
-// this is not working i'll fix tmr
-TEST_CASE("moduleresolver_resolve_module_hard")
-{
-    std::string root = getLuteProjectRootAbsolute();
-    std::string requirer = joinPaths(root, "tests/src/resolver/mainmodule.luau");
-    Luau::LuteModuleResolver resolver;
-
-    // Create a ModuleInfo for the requirer
-    Luau::ModuleInfo context;
-    context.name = requirer;
-
-    // Create an AstExprConstantString for a more complex require argument
-    Luau::AstExprConstantString requireExpr;
-    std::string requirePath = "@std/tableext";
-    requireExpr.value = Luau::AstString{requirePath.data(), static_cast<unsigned>(requirePath.size())};
-
-    auto moduleInfoOpt = resolver.resolveModule(&context, &requireExpr);
-    // We accept either a resolution or a graceful failure with an empty optional
-    CHECK(moduleInfoOpt || true);
-}
+// TODO: add tests for resolveModule
