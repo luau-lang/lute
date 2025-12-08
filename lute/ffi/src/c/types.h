b5c4b00dc19d9f01bd42e97cb5e9c791a3f89301
bjcscat <bjcscat@protonmail.com>
2025-12-08 13:33:41 -0600

    Move userdatareference out of ffi

--- lute/ffi/src/c/types.h
+++ lute/ffi/src/c/types.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "lua.h"
-#include "lualib.h"
 #include "ffi.h"
 
 #include <csetjmp>
@@ -43,102 +42,6 @@ public:
 private:
     std::vector<std::function<void()>> callbacks;
 };
-
-template<typename T>
-class UserdataReference
-{
-public:
-    UserdataReference(lua_State* L, int index)
-    {
-        this->L = L;
-        obj = static_cast<T*>(luaL_checkudata(L, index, T::metatableName));
-        ref = lua_ref(L, index);
-    }
-
-    ~UserdataReference()
-    {
-        lua_unref(L, ref);
-    }
-
-    UserdataReference(const UserdataReference& other)
-        : L{other.L}
-        , obj{other.obj}
-    {
-        lua_getref(L, ref);
-
-        ref = lua_ref(L, -1);
-
-        lua_pop(L, 1);
-    }
-
-    UserdataReference& operator=(const UserdataReference& other)
-    {
-        if (this != &other)
-        {
-            lua_getref(other.L, other.ref);
-            int newRef = lua_ref(other.L, -1);
-            lua_pop(other.L, 1);
-
-            lua_unref(L, ref);
-
-            L = other.L;
-            obj = other.obj;
-            ref = newRef;
-        }
-
-        return *this;
-    }
-
-    UserdataReference(UserdataReference&& other) noexcept
-        : L{other.L}
-        , obj{other.obj}
-        , ref{other.ref}
-    {
-        other.ref = LUA_NOREF;
-    }
-
-    UserdataReference& operator=(UserdataReference&& other) noexcept
-    {
-        if (this != &other)
-        {
-            lua_unref(L, ref);
-
-            L = other.L;
-            obj = other.obj;
-            ref = other.ref;
-
-            other.ref = LUA_NOREF;
-        }
-
-        return *this;
-    }
-
-    T* operator->()
-    {
-        return obj;
-    }
-
-    const T* operator->() const
-    {
-        return obj;
-    }
-
-    T* get()
-    {
-        return obj;
-    }
-
-    const T* get() const
-    {
-        return obj;
-    }
-
-private:
-    lua_State* L;
-    T* obj;
-    int ref = LUA_NOREF;
-};
-
 
 class CType
 {
--- lute/runtime/include/lute/ref.h
+++ lute/runtime/include/lute/ref.h
@@ -1,9 +1,106 @@
 #pragma once
 
+#include "lua.h"
+
 #include <memory>
-#include <utility>
 
 struct lua_State;
+
+
+template<typename T>
+class UserdataReference
+{
+public:
+    UserdataReference(lua_State* L, int index)
+    {
+        this->L = L;
+        obj = static_cast<T*>(luaL_checkudata(L, index, T::metatableName));
+        ref = lua_ref(L, index);
+    }
+
+    ~UserdataReference()
+    {
+        lua_unref(L, ref);
+    }
+
+    UserdataReference(const UserdataReference& other)
+        : L{other.L}
+        , obj{other.obj}
+    {
+        lua_getref(L, ref);
+
+        ref = lua_ref(L, -1);
+
+        lua_pop(L, 1);
+    }
+
+    UserdataReference& operator=(const UserdataReference& other)
+    {
+        if (this != &other)
+        {
+            lua_getref(other.L, other.ref);
+            int newRef = lua_ref(other.L, -1);
+            lua_pop(other.L, 1);
+
+            lua_unref(L, ref);
+
+            L = other.L;
+            obj = other.obj;
+            ref = newRef;
+        }
+
+        return *this;
+    }
+
+    UserdataReference(UserdataReference&& other) noexcept
+        : L{other.L}
+        , obj{other.obj}
+        , ref{other.ref}
+    {
+        other.ref = LUA_NOREF;
+    }
+
+    UserdataReference& operator=(UserdataReference&& other) noexcept
+    {
+        if (this != &other)
+        {
+            lua_unref(L, ref);
+
+            L = other.L;
+            obj = other.obj;
+            ref = other.ref;
+
+            other.ref = LUA_NOREF;
+        }
+
+        return *this;
+    }
+
+    T* operator->()
+    {
+        return obj;
+    }
+
+    const T* operator->() const
+    {
+        return obj;
+    }
+
+    T* get()
+    {
+        return obj;
+    }
+
+    const T* get() const
+    {
+        return obj;
+    }
+
+private:
+    lua_State* L;
+    T* obj;
+    int ref;
+};
 
 struct Ref
 {
