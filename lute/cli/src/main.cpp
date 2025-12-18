69be37a70f98cf839deb04e61547c481567c9a13
Nick Winans <nick@winans.io>
2025-12-18 11:36:33 -0800

    don't buffer lol

--- lute/cli/src/main.cpp
+++ lute/cli/src/main.cpp
@@ -2,8 +2,15 @@
 #include "lute/clireporter.h"
 #include "lute/uvstate.h"
 
+#include <cstdio>
+
 int main(int argc, char** argv)
 {
+    // Disable buffering on stdout/stderr so output appears immediately
+    // when running as a subprocess (e.g., from C# Process or other hosts)
+    setvbuf(stdout, nullptr, _IONBF, 0);
+    setvbuf(stderr, nullptr, _IONBF, 0);
+
     UvGlobalState uvState(argc, argv);
     CLIReporter reporter;
     return cliMain(argc, argv, reporter);
