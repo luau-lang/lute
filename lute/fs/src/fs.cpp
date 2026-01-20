04c549105ad586a6a94dbcf6cc6e49a0259d606a
Annie Tang <98965493+annieetang@users.noreply.github.com>
2026-01-20 11:03:03 -0800

    remove commented out stuff

--- lute/fs/src/fs.cpp
+++ lute/fs/src/fs.cpp
@@ -51,27 +51,6 @@
 namespace fs
 {
 
-// // TOOD: move to fs_impl once all functions are refactored and made async
-// const char* UV_TYPENAME_UNKNOWN = "unknown"; // UV_DIRENT_UNKNOWN
-// const char* UV_TYPENAME_FILE = "file";       // UV_DIRENT_FILE
-// const char* UV_TYPENAME_DIR = "dir";         // UV_DIRENT_DIR
-// const char* UV_TYPENAME_LINK = "link";       // UV_DIRENT_LINK
-// const char* UV_TYPENAME_FIFO = "fifo";       // UV_DIRENT_FIFO
-// const char* UV_TYPENAME_SOCKET = "socket";   // UV_DIRENT_SOCKET
-// const char* UV_TYPENAME_CHAR = "char";       // UV_DIRENT_CHAR
-// const char* UV_TYPENAME_BLOCK = "block";     // UV_DIRENT_BLOCK
-
-// const char* UV_DIRENT_TYPES[] = {
-//     UV_TYPENAME_UNKNOWN,
-//     UV_TYPENAME_FILE,
-//     UV_TYPENAME_DIR,
-//     UV_TYPENAME_LINK,
-//     UV_TYPENAME_FIFO,
-//     UV_TYPENAME_SOCKET,
-//     UV_TYPENAME_CHAR,
-//     UV_TYPENAME_BLOCK,
-// };
-
 static UVFile* getFileHandle(lua_State* L, int index)
 {
     if (!lua_islightuserdata(L, index))
