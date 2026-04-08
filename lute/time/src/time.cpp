12f3e69bbe02938f4d4c8957f524afae12291bc4
Ariel Weiss <arielweiss@roblox.com>
2026-04-08 13:22:38 -0700

    initialize the duration library when initializing fs

--- lute/time/src/time.cpp
+++ lute/time/src/time.cpp
@@ -460,9 +460,13 @@ int lua_since(lua_State* L)
 }
 } // namespace libtime
 
-static void init_duration_lib(lua_State* L)
+void init_duration_lib(lua_State* L)
 {
-    luaL_newmetatable(L, kDurationType);
+    if (luaL_newmetatable(L, kDurationType) == 0)
+    {
+        lua_pop(L, 1);
+        return;
+    }
 
     // Protect metatable from being changed
     lua_pushstring(L, "The metatable is locked");
