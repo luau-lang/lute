21f09f51c89051358dd7ddc18d3d5ca0d1c865db
green <obviouslygreen.official@gmail.com>
2026-04-18 18:20:47 +0200

    Add FFI subproject and minimal library
    Add a new lute/ffi static library and register it in the top-level CMakeLists.
    
    Provide minimal FFI scaffolding: an FFI LuteLibrary with luaopen_ffi and luteopen_ffi entry points, an empty luaL_Reg table, and a readonly table with a "c" property. This sets up the FFI submodule for further implementation.

---
+++ lute/ffi/include/lute/ffi.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "lute/library.h"
+
+#include "lua.h"
+#include "lualib.h"
+
+#include <string>
+
+// open the library as a standard global luau library
+int luaopen_ffi(lua_State* L);
+// open the library as a table on top of the stack
+int luteopen_ffi(lua_State* L);
+
+struct FFI : LuteLibrary<FFI>
+{
+    static constexpr const char kName[] = "ffi";
+    static int pushLibrary(lua_State* L);
+    static const luaL_Reg lib[];
+    static const char* const properties[];
+};
