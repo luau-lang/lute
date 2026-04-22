882ea836744d86deec055fcc83e29280e50106b3
Nick Winans <nick@winans.io>
2026-04-21 19:29:48 -0700

    Parallel test runners

--- lute/vm/src/spawn.cpp
+++ lute/vm/src/spawn.cpp
@@ -1,5 +1,6 @@
 #include "lute/vm.h"
 
+#include "lute/clivfs.h"
 #include "lute/ref.h"
 #include "lute/require.h"
 #include "lute/requirevfs.h"
@@ -188,7 +189,8 @@ static void* createChildVmRequireContext(lua_State* L)
     if (!ctx)
         luaL_error(L, "unable to allocate RequireCtx");
 
-    ctx = new (ctx) RequireCtx{std::make_unique<RequireVfs>()};
+    // Explicit CliVfs should be removed with https://github.com/luau-lang/lute/issues/1027
+    ctx = new (ctx) RequireCtx{std::make_unique<RequireVfs>(CliVfs{})};
 
     // Store RequireCtx in the registry to keep it alive for the lifetime of
     // this lua_State. Memory address is used as a key to avoid collisions.
@@ -216,8 +218,9 @@ int VM::lua_spawn(lua_State* L)
     lua_Debug ar;
     lua_getinfo(L, 1, "s", &ar);
 
-    // Require the target module
-    RequireCtx ctx{std::make_unique<RequireVfs>()};
+    // Require the target module.
+    // Explicit CliVfs should be removed with https://github.com/luau-lang/lute/issues/1027
+    RequireCtx ctx{std::make_unique<RequireVfs>(CliVfs{})};
     luarequire_pushproxyrequire(child->GL, requireConfigInit, &ctx);
     lua_pushstring(child->GL, file);
     lua_pushstring(child->GL, ar.source);
