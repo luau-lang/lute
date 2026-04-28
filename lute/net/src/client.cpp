524259b21c5c60142c3c52a1703127ccc5c3b3ce
Ariel Weiss <arielweiss@roblox.com>
2026-04-28 15:41:10 -0700

    data can be undefined, is breaking ci

--- lute/net/src/client.cpp
+++ lute/net/src/client.cpp
@@ -265,7 +265,7 @@ int request(lua_State* L)
                     lua_createtable(L, 0, 4);
 
                     lua_pushstring(L, "body");
-                    lua_pushlstring(L, resp.body.data(), resp.body.size());
+                    lua_pushlstring(L, resp.body.empty() ? "" : resp.body.data(), resp.body.size());
                     lua_settable(L, -3);
 
                     lua_pushstring(L, "headers");
