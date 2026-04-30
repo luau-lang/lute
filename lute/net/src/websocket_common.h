2ea06763eb881c380f21381fafb99d366c983488
Vighnesh Vijay <vvijay@roblox.com>
2026-04-30 16:30:38 -0700

    Tidy up websockets?

---
+++ lute/net/src/websocket_common.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "lua.h"
+
+#include <cstddef>
+
+namespace net
+{
+
+struct WebSocketPayload
+{
+    const char* data = nullptr;
+    size_t length = 0;
+    bool binary = false;
+};
+
+WebSocketPayload extractWebSocketPayload(lua_State* L, int index);
+
+} // namespace net
