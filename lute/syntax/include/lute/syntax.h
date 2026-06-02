10191118cdcf16da156ca6e8d6b8629cdda6bc8d
Sora Kanosue <kanosuesora@gmail.com>
2026-06-02 15:05:28 -0700

    Splits syntax library out from luau (#1149)
    The Luau library is getting rather large to work with and is will be
    super unwieldy to work with if I add the AST types in the same files.
    This PR splits out syntax-specific items from `@lute/luau` into a new
    `@lute/syntax` library. I also split out the CST types into their own
    submodule on the `@std` side.
    
    I plan to move the parsing library into `@lute/syntax` in a followup

---
+++ lute/syntax/include/lute/syntax.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "lute/library.h"
+
+#include "lua.h"
+#include "lualib.h"
+
+namespace luau
+{
+static constexpr const char kSpanType[] = "span"; // Uncapitalized because the span library lives under `cst.{kSpanType}`
+
+int ltSpan(lua_State* L);
+} // namespace luau
+
+// open the library as a standard global luau library
+LUTE_API int luaopen_syntax(lua_State* L);
+// open the library as a table on top of the stack
+LUTE_API int luteopen_syntax(lua_State* L);
+
+struct Syntax : LuteLibrary<Syntax>
+{
+    static constexpr const char kName[] = "syntax";
+    static int pushLibrary(lua_State* L);
+    static const luaL_Reg lib[];
+    static const char* const properties[];
+};
