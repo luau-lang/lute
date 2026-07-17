08af54031c28850a0320d3151e66f28015f2c01b
Jonathan <jkelaty@roblox.com>
2026-07-17 13:16:49 -0700

    add tests

--- tests/src/typecheck.test.cpp
+++ tests/src/typecheck.test.cpp
@@ -32,3 +32,42 @@ TEST_CASE_FIXTURE(LuteFixture, "typecheck_user_code_doesnt_pass_with_batteries")
     auto result = typecheck({testFilePath}, getReporter());
     CHECK(result == 1);
 }
+
+TEST_CASE_FIXTURE(LuteFixture, "typecheck_with_definitions_succeeds")
+{
+    std::string luteProjectRoot = getLuteProjectRootAbsolute();
+    std::string testFilePath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_success/user_code.luau");
+    std::string configPath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_success/.config.luau");
+
+    auto result = typecheck({testFilePath}, getReporter(), configPath);
+    CHECK(result == 0);
+}
+
+TEST_CASE_FIXTURE(LuteFixture, "typecheck_without_definitions_fails")
+{
+    std::string luteProjectRoot = getLuteProjectRootAbsolute();
+    std::string testFilePath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_success/user_code.luau");
+
+    auto result = typecheck({testFilePath}, getReporter());
+    CHECK(result == 1);
+}
+
+TEST_CASE_FIXTURE(LuteFixture, "typecheck_definition_file_not_found")
+{
+    std::string luteProjectRoot = getLuteProjectRootAbsolute();
+    std::string testFilePath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_missing/user_code.luau");
+    std::string configPath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_missing/.config.luau");
+
+    auto result = typecheck({testFilePath}, getReporter(), configPath);
+    CHECK(result == 1);
+}
+
+TEST_CASE_FIXTURE(LuteFixture, "typecheck_definition_file_parse_error")
+{
+    std::string luteProjectRoot = getLuteProjectRootAbsolute();
+    std::string testFilePath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_invalid/user_code.luau");
+    std::string configPath = joinPaths(luteProjectRoot, "tests/src/typecheck/definitions_invalid/.config.luau");
+
+    auto result = typecheck({testFilePath}, getReporter(), configPath);
+    CHECK(result == 1);
+}
