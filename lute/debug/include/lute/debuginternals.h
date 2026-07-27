ab2194e9a0f6fe9b1d447f1601a5e8064fb08454
William Zhao <williamzhao@roblox.com>
2026-07-27 11:42:47 -0700

    move threads to own branch

--- lute/debug/include/lute/debuginternals.h
+++ lute/debug/include/lute/debuginternals.h
@@ -10,6 +10,7 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 struct lua_State;
@@ -35,15 +36,25 @@ struct Breakpoint
     explicit Breakpoint(int id, std::string sourcePath, int line, BreakpointStatus status);
 };
 
+// Each Thread represents one coroutine in our Lute runtime.
+struct Thread
+{
+    int id = -1;
+    std::string name;
+    Thread() = default; // for unordered_map
+    Thread(int id, std::string name);
+    bool operator==(const Thread& other) const;
+};
+
 struct LaunchConfig
 {
     // onBreakpointInstall is called whenever an installation attempt is actually made, regardless
     // of whether it resulted in being installed or the bp being invalid.
     std::function<void(const Breakpoint& bp)> onBreakpointInstall;
     std::function<void(const Breakpoint& bp)> onBreakpointUninstall;
-    std::function<void(const Breakpoint& bp)> onBreakpointHit;
+    std::function<void(const Thread& thread, const Breakpoint& bp)> onBreakpointHit;
     std::function<void(bool success)> onExit;
-    std::function<void()> onPause;
+    std::function<void(const Thread& thread)> onPause;
 };
 
 struct Target
@@ -74,6 +85,9 @@ struct Target
     std::optional<Breakpoint> getBreakpointById(int breakpointId) const;
     std::optional<Breakpoint> getBreakpointBySourceLine(std::string source, int line) const;
 
+    // For inspection while paused:
+    std::vector<Thread> getThreads() const;
+
     // For actively running scripts:
     bool launch(std::string sourcePath, const std::vector<std::string>& args, LaunchConfig config = {});
     bool continueProcess();
@@ -92,7 +106,7 @@ private:
     bool paused = true;
     bool launched = false;
     std::unordered_map<int, Breakpoint> breakpoints; // breakpoint id -> breakpoint object (this is unordered_map to support erase)
-    bool continueRequestedBp = false;
+    std::unordered_set<lua_State*> continueRequestedBp; // if the thread's lua_State* is in this set, we skip the next bp it hits
     std::optional<Breakpoint> bpHit;
     LaunchConfig launchConfig;
 
@@ -107,6 +121,11 @@ private:
     // for require contexts
     std::unique_ptr<RequireCtx> requireCtx;
 
+    // thread information
+    int threadId = 0;
+    std::unordered_map<lua_State*, Thread> stateToThread; // lua_State* -> thread information about that state
+    std::unordered_map<int, lua_State*> threadIdToState;  // thread id -> lua_State*
+
     // private methods are meant for internal calls, so these don't lock targetMutex
     std::optional<Breakpoint> getBreakpointBySourceLineHelper(std::string source, int line) const;
     std::optional<Breakpoint> getBreakpointByIdHelper(int breakpointId) const;
@@ -117,5 +136,6 @@ private:
 
     void installBpHitCallback();
     void installExitCallback();
+    void installThreadCallback();
 };
 } // namespace debug
