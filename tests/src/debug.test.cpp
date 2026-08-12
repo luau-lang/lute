59158e2a2bb12b10e7f529dc8627f8243367c6e7
William Zhao <williamzhao@roblox.com>
2026-08-12 15:45:56 -0700

    fix stuff for proto

--- tests/src/debug.test.cpp
+++ tests/src/debug.test.cpp
@@ -710,7 +710,7 @@ TEST_SUITE("Debug")
     {
         std::string fixturePath = getDebugFixturePath("variables.luau");
         Target target(*runtime);
-        target.setBreakpoint(fixturePath, 10);
+        target.setBreakpoint(fixturePath, 15);
 
         std::vector<Variable> capturedLocals;
         config.onBreakpointHit = [&](const Thread&, const Breakpoint&)
@@ -863,8 +863,8 @@ TEST_SUITE("Debug")
         std::string fixturePath = getDebugFixturePath("variables.luau");
         Target target(*runtime);
         Breakpoint bp1 = target.setBreakpoint(fixturePath, 6);
-        Breakpoint bp2 = target.setBreakpoint(fixturePath, 9);
-        Breakpoint bp3 = target.setBreakpoint(fixturePath, 10);
+        Breakpoint bp2 = target.setBreakpoint(fixturePath, 14);
+        Breakpoint bp3 = target.setBreakpoint(fixturePath, 15);
         config.onBreakpointHit = [&](const Thread&, const Breakpoint& bp)
         {
             const std::vector<Thread>& threads = target.getThreads();
