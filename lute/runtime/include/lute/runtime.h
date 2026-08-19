82588756879b5606d0873b1eccc4468f57776d76
William Zhao <williamzhao@roblox.com>
2026-08-19 13:34:34 -0700

    add tests

--- lute/runtime/include/lute/runtime.h
+++ lute/runtime/include/lute/runtime.h
@@ -144,6 +144,7 @@ struct Runtime
     std::atomic<int> numLaunchedDebuggees = 0;
     void stopDebug();
     void continueDebug();
+    void waitForDebugContinue();
     // Same as scheduleLuauCallback but we since we don't call this within a libuv completion callback
     // we need to wake up the libuv event loop.
     void scheduleDebugLuauCallback(std::shared_ptr<Ref> callbackRef, std::function<int(lua_State*)> argPusher);
