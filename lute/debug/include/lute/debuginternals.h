eae0c5725bbd6cad63c0f379a739a2c6a0f778e3
William Zhao <williamzhao@roblox.com>
2026-08-20 11:14:57 -0700

    unfix to const std::string&

--- lute/debug/include/lute/debuginternals.h
+++ lute/debug/include/lute/debuginternals.h
@@ -183,7 +183,7 @@ struct Target
     std::optional<std::vector<Variable>> getVariablesByScopeType(int frameId, VariableScopeType contextType);
 
     // For evaluation:
-    EvaluateResult evaluateExpression(const std::string& expression, int frameId = -1);
+    EvaluateResult evaluateExpression(std::string expression, int frameId = -1);
 
     // For actively running scripts:
     std::optional<std::string> launch(std::string sourcePath, const std::vector<std::string>& args, LaunchConfig config = {});
@@ -259,7 +259,7 @@ private:
     std::optional<StackFrame> getStackFrameHelper(int threadId, int level);
     std::optional<std::vector<VariableScope>> getScopesHelper(int threadId, int level);
     std::optional<std::vector<Variable>> getVariablesHelper(int varRef);
-    EvaluateResult evaluateExpressionHelper(lua_State* L, int level, const std::string& expression);
+    EvaluateResult evaluateExpressionHelper(lua_State* L, int level, std::string expression);
     void continueProcessHelper();
 
     bool installBreakpoint(lua_State* L, Breakpoint& bp);
